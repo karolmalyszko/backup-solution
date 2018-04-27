126bacc133e5bf9256609b61583b808c6de12f1a
Karol Małyszko <k.malyszko@bitcraft.com.pl>
2018-04-27 12:29:49 +0200

    Added logging to file.

--- Helpers.py
+++ Helpers.py
@@ -1,7 +1,13 @@
 import os
 import settings
 import datetime
-
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%d-%m-%y %H:%M:%S',
+                    filename='/var/log/backup-solution.log',
+                    filemode='a'
+                    )
 DEBUG = settings.debug
 
 
@@ -9,12 +15,12 @@ def directoryHelper(retentionTime, backupDestination):
     # check if backupDestination exists
     if not os.path.isdir(backupDestination):
         if DEBUG:
-            print("Creating 'backupDestination' directory")
+            logging.debug("Creating 'backupDestination' directory")
         os.system("mkdir " + backupDestination)
     else:
         # rotate backup directories
         if DEBUG:
-            print("Rotating directories")
+            logging.debug("Rotating directories")
         directoryRotator(retentionTime, backupDestination)
     return 0
 
@@ -23,12 +29,12 @@ def directoryRotator(retentionTime, backupDestination):
     # remove oldest snapshot
     if os.path.isdir(backupDestination + 'backup.' + str(retentionTime)):
         if DEBUG:
-            print("Removing oldest backup dir")
+            logging.debug("Removing oldest backup dir")
         os.system("rm -rf " + backupDestination + "backup." + str(retentionTime))
 
     # rotate backups one tier down
     if DEBUG:
-        print("Rotating backups")
+        logging.debug("Rotating backups")
     for x in range(int(retentionTime) - 1, -1, -1):
         if os.path.isdir(backupDestination + 'backup.' + str(x)):
             os.system('mv ' + backupDestination + 'backup.' + str(x) + ' ' + backupDestination + 'backup.' + str(x+1))
@@ -38,7 +44,7 @@ def directoryRotator(retentionTime, backupDestination):
 def createBackup0(backupDestination):
     # create backup.0 from .sync
     if DEBUG:
-        print("Creating backup.0")
+        logging.debug("Creating backup.0")
     os.system("cp -al " + backupDestination + ".sync " + backupDestination + "backup.0")
     os.system("echo {} > {}backup.0/{}.timestamp".format(datetime.datetime.now().strftime("%d-%m-%Y"),
                                                          backupDestination,
@@ -49,12 +55,12 @@ def createBackup0(backupDestination):
 def configurationValidator(configuration):
     # check if backupDestination key exists
     if "backupDestination" not in configuration:
-        print("No backupDestination key present. Setting to default.")
+        logging.warning("No backupDestination key present. Setting to default.")
         configuration["backupDestination"] = settings.defaultBackupDestination
 
     # check if backupDestination ends in /
     if not str(configuration["backupDestination"]).endswith("/"):
-        print("Incorrect 'backupDestination' path, must end with /")
+        logging.error("Incorrect 'backupDestination' path, must end with /")
         exit(3)
 
     # check if user key exists; not sure if it is really necessary, may be included in backupSource key
@@ -64,25 +70,25 @@ def configurationValidator(configuration):
             # check if user exists when host is not localhost
             if str(configuration["host"]) is not "localhost":
                 if str(configuration["user"]) is "":
-                    print("No user present. This ain't gonna work. Fix it!")
+                    logging.error("No user present. This ain't gonna work. Fix it!")
                     exit(4)
         else:
-            print("No host present, but user is. This does not look right. Think it trough. Terminating.")
+            logging.error("No host present, but user is. This does not look right. Think it trough. Terminating.")
             exit(5)
 
     # check if backupSource key exists
     if "backupSource" not in configuration:
-        print("Nothng to backup. Really?")
+        logging.error("Nothng to backup. Really?")
         exit(6)
 
     # check if command key exists
     if "command" not in configuration:
-        print("Setting command to defaults.")
+        logging.warning("Setting command to defaults.")
         configuration["command"] = settings.defaultCommand
 
     # check if arguments key exists
     if "arguments" not in configuration:
-        print("Setting arguments to defaults.")
+        logging.warning("Setting arguments to defaults.")
         configuration["arguments"] = settings.defaultArguments
 
     return configuration
